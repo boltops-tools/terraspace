6c1ff9241caf592da00241ab5fa307cd0cefce6d
Tung Nguyen <tongueroo@gmail.com>
2020-05-30 21:37:00 +0000

    fix force option

--- lib/terraspace/cli/new/test/base.rb
+++ lib/terraspace/cli/new/test/base.rb
@@ -3,6 +3,14 @@ module Terraspace::CLI::New::Test
     include Thor::Actions
     include Terraspace::CLI::New::Helper
 
+    def self.base_options
+      [
+        [:force, type: :boolean, desc: "Bypass overwrite are you sure prompt for existing files"],
+      ]
+    end
+
+    base_options.each { |args| class_option(*args) }
+
   private
     def test_template_source(type)
       source = Terraspace::CLI::New::Source::Test.new(self, @options)
