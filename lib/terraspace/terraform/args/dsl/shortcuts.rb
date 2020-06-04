a98dfde9241e34030f1ac8d1ade77783f82f5ec2
Tung Nguyen <tongueroo@gmail.com>
2020-06-04 16:42:57 +0000

    add cli args with_* shortcuts

---
+++ lib/terraspace/terraform/args/dsl/shortcuts.rb
@@ -0,0 +1,47 @@
+module Terraspace::Terraform::Args::Dsl
+  module Shortcuts
+    COMMANDS_WITH_INPUT = %w[
+      apply
+      import
+      init
+      plan
+      refresh
+    ]
+
+    COMMANDS_WITH_LOCKING = %w[
+      apply
+      destroy
+      import
+      init
+      plan
+      refresh
+      taint
+      untaint
+    ]
+
+    COMMANDS_WITH_PARALLELISM = %w[
+      apply
+      plan
+      destroy
+    ]
+
+    COMMANDS_WITH_VARS = %w[
+      apply
+      console
+      destroy
+      import
+      plan
+      push
+      refresh
+    ]
+
+    def shortcuts
+      {
+        with_input: COMMANDS_WITH_INPUT,
+        with_locking: COMMANDS_WITH_LOCKING,
+        with_parallelism: COMMANDS_WITH_PARALLELISM,
+        with_vars: COMMANDS_WITH_VARS,
+      }
+    end
+  end
+end
