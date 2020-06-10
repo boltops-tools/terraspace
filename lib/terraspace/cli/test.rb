a7069298d20f26456f56763608c1c1530384c310
Tung Nguyen <tongueroo@gmail.com>
2020-06-10 00:13:54 +0000

    cd into test folder if at root of module foler and running terraspace test

--- lib/terraspace/cli/test.rb
+++ lib/terraspace/cli/test.rb
@@ -11,8 +11,25 @@ class Terraspace::CLI
     end
 
     def execute(command)
+      command = adjust_command(command)
       puts "=> #{command}"
       Kernel.exec(command)
+    end
+
+    def adjust_command(command)
+      if cd_into_test?
+        command = "bundle exec #{command}" unless command.include?("bundle exec")
+        command = "cd test && #{command}"
+      else
+        command
+      end
+    end
+
+    # Automatically cd into the test folder in case running within the root of a module.
+    # Detect/guess that we're in a module folder vs the terraspace project
+    def cd_into_test?
+      !File.exist?("app") && File.exist?("test") &&
+      (File.exist?("main.tf") || File.exist?("main.rb"))
     end
   end
 end
