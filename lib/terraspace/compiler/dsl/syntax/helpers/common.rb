95672892365e47b61aed7aca1da0292b2995d5d5
Tung Nguyen <tongueroo@gmail.com>
2020-07-20 23:32:52 +0000

    add terraspace_command helper

---
+++ lib/terraspace/compiler/dsl/syntax/helpers/common.rb
@@ -0,0 +1,28 @@
+module Terraspace::Compiler::Dsl::Syntax::Helpers
+  module Common
+    extend Memoist
+
+    Meta = Terraspace::Compiler::Dsl::Meta
+
+    def var
+      Meta::Var.new
+    end
+    memoize :var
+
+    def local
+      Meta::Local.new
+    end
+    memoize :local
+
+    # Only show the first 2 args and now the options. Examples:
+    #
+    #     terraspace_command => "terraspace up demo"
+    #     terraspace_command('-') => "terraspace-up-demo"
+    #
+    def terraspace_command(separator=' ')
+      args = ARGV[0..1] || []
+      command = ["terraspace"] + args
+      command.join(separator)
+    end
+  end
+end
--- lib/terraspace/compiler/dsl/syntax/mod.rb
+++ lib/terraspace/compiler/dsl/syntax/mod.rb
@@ -1,5 +1,6 @@
 module Terraspace::Compiler::Dsl::Syntax
   module Mod
     include_dir("mod")
+    include_dir("helpers")
   end
 end
