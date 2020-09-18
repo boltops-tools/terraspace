7dd3c951ceb414d07a4a770f4da9043e244f89b2
Tung Nguyen <tongueroo@gmail.com>
2020-09-18 22:28:00 +0000

    fix backend auto creation

--- lib/terraspace/cli/commander.rb
+++ lib/terraspace/cli/commander.rb
@@ -7,18 +7,8 @@ class Terraspace::CLI
 
     def run
       Terraspace::Builder.new(@options).run unless @options[:build] # Up already ran build
-      auto_create_backend
       Init.new(@options.merge(calling_command: @name)).run
       Terraspace::Terraform::Runner.new(@name, @options).run
-    end
-
-    def auto_create_backend
-      return unless auto_create?
-      Terraspace::Compiler::Backend.new(@mod).create
-    end
-
-    def auto_create?
-      @name == "apply" || @name == "plan" && @options[:yes]
     end
   end
 end
