01ac6d700c2f2057f265f1d0e42b626f33f74b69
Tung Nguyen <tongueroo@gmail.com>
2020-10-23 14:05:05 +0000

    add info --path option

--- lib/terraspace/cli/info.rb
+++ lib/terraspace/cli/info.rb
@@ -3,12 +3,24 @@ class Terraspace::CLI
     extend Memoist
 
     def run
+      if @options[:path]
+        show_path
+      else
+        show_all
+      end
+    end
+
+    def show_all
       presenter = CliFormat::Presenter.new(@options)
       presenter.header = %w[Name Value]
       info.each do |k,v|
         presenter.rows << [k, v]
       end
       presenter.show
+    end
+
+    def show_path
+      puts info[:cache_dir]
     end
 
     def info
