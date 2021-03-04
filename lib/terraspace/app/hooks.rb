10dc540e0d7759e9819bfc0fa95beaf48de40baf
Tung Nguyen <tongueroo@gmail.com>
2021-03-04 20:05:58 +0000

    new and improved boot hooks interface
    * warn about config.hooks.on_boot removal

--- lib/terraspace/app/hooks.rb
+++ lib/terraspace/app/hooks.rb
@@ -1,18 +1,18 @@
 class Terraspace::App
   class Hooks
-    class_attribute :hooks
-    self.hooks = {}
-
+    # Terraspace.configure do |config|
+    #   config.hooks.on_boot do
+    #     ENV['AWS_PROFILE'] = 'dev'
+    #   end
+    # end
     def on_boot(&block)
-      self.class.hooks[:on_boot] = block
-    end
-
-    class << self
-      def run_hook(name)
-        name = name.to_sym
-        hook = hooks[name]
-        hook.call if hook
-      end
+      # Using puts instead of logger.info because on_boot gets assigned super-early only
+      # and logger is set as part of the config/app.rb also. Using puts avoids
+      # infinite loop. Will remove this file completely in a future patch release anyway.
+      puts <<~EOL.color(:yellow)
+        WARN: config.on_boot has been removed. Create a config/boot.rb instead
+        See: https://terraspace.cloud/docs/config/boot/
+      EOL
     end
   end
 end
