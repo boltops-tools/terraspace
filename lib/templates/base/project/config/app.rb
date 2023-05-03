fd8e972678bbb196b3da24b2e698e52c21453be3
Tung Nguyen <tongueroo@gmail.com>
2023-05-03 12:30:23 +0000

    update new project generator config/app.rb config.build.copy_modules

--- lib/templates/base/project/config/app.rb
+++ lib/templates/base/project/config/app.rb
@@ -2,10 +2,7 @@
 Terraspace.configure do |config|
   # config.logger.level = :info
 
-  # To enable Terraspace Cloud set config.cloud.org
-  # config.cloud.org = "ORG"          # required: replace with your org. only letters, numbers, underscore and dashes allowed
-  # config.cloud.project = "main"     # optional. main is the default project name. only letters, numbers, underscore and dashes allowed
-
-  # Uncomment to enable Cost Estimation. See: http://terraspace.cloud/docs/cloud/cost-estimation/
-  # config.cloud.cost.enabled = true
+  # copy_modules setting introduced 2.2.5 to speed up terraspace build
+  # See: https://terraspace.cloud/docs/config/reference
+  config.build.copy_modules = true
 end
