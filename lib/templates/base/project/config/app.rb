ac7c7d2a34015db0407ce9ac1e075983b30d455a
Tung Nguyen <tongueroo@gmail.com>
2023-08-29 22:01:17 +0000

    grammar fixes

--- lib/templates/base/project/config/app.rb
+++ lib/templates/base/project/config/app.rb
@@ -2,7 +2,7 @@
 Terraspace.configure do |config|
   # config.logger.level = :info
 
-  # copy_modules setting introduced 2.2.5 to speed up terraspace build
+  # copy_modules setting introduced in 2.2.5 to speed up terraspace build
   # See: https://terraspace.cloud/docs/config/reference
   config.build.copy_modules = true
 
